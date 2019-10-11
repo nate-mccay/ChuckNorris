caceb7baeab37de8ef5e8f466cd016f7d5750b84
nate mccay <nate.mccay@ymail.com>
2019-10-11 11:45:40 -0500

    code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.chucknorris.io/jokes/random";
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync(url).Result;
+            string joke2 = JObject.Parse(response).GetValue("value").ToString();
+            Console.WriteLine(joke2);
+        }
+    }
+}
